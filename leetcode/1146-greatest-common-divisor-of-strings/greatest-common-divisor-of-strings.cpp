94b3bfec5cc26dc916c42ad36bec52325be12490
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-07-21 21:06:54 +1000

    Update greatest-common-divisor-of-strings.cpp

--- leetcode/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ leetcode/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -3,6 +3,7 @@ public:
     // uses gcd = greatest common divisor 
     // uses substr = substring
     // ? = ternary operator used to return different values based on (str1 + str2 == str2 + str1) is true or false, : the line after matches the ?
+    // gcd() function: O(log(min(n,m))), Checking if the two strings are equal: O(1), Extracting the substring: O(1) Therefore, the total run time of the code is O(log(min(n,m))).
 
     string gcdOfStrings(string str1, string str2) {
         return (str1 + str2 == str2 + str1)?
