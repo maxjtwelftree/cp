16aa30450189ab47a67432c8bb23649c712d9df5
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-07-22 21:33:26 +1000

    Create breadth-first-search.cpp

---
+++ templates/breadth-first-search.cpp
@@ -0,0 +1,39 @@
+// Title Breadth First Search Function
+// Parameters: graph, start
+// Returns: A list of all vertices in the graph that can be reached from the starting vertex start
+// Description: Performs a breadth-first search on a graph starting from the given vertex start
+
+// Definition for a tree node
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+// Breadth-First Search for Trees (Level Order Traversal)
+// What is actually used
+void bfsTree(TreeNode* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    std::queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* current = q.front();
+        q.pop();
+
+        std::cout << current->val << " ";
+
+        if (current->left != nullptr) {
+            q.push(current->left);
+        }
+
+        if (current->right != nullptr) {
+            q.push(current->right);
+        }
+    }
+}
+ 
