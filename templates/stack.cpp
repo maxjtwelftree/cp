0d5cb1efb39caae52969a8eb644ec1931324c19c
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-07-22 22:09:31 +1000

    Create stack.cpp

---
+++ templates/stack.cpp
@@ -0,0 +1,36 @@
+// Title: Stack
+// Parameters: None
+// Returns: A stack data structure that can be used to push and pop elements
+// Description: Implements a basic stack data structure that can be used to solve problems involving stacks
+// Time Complexity = O(1)
+// Space Complexity = O(n)
+// A template for a stack
+
+template <typename T>
+class Stack {
+private:
+    vector<T> data;
+
+public:
+    // Push an element onto the stack
+    void push(T element) {
+        data.push_back(element);
+    }
+
+    // Pop an element from the stack
+    T pop() {
+        T element = data.back();
+        data.pop_back();
+        return element;
+    }
+
+    // Check if the stack is empty
+    bool empty() {
+        return data.empty();
+    }
+
+    // Get the size of the stack
+    int size() {
+        return data.size();
+    }
+};
