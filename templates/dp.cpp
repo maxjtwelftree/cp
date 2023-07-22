2e78a22224863718387d6c0b6aed8aea4e739b68
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-07-22 21:40:30 +1000

    Update dp.cpp

--- templates/dp.cpp
+++ templates/dp.cpp
@@ -4,8 +4,6 @@
 // Description: Solves a problem by breaking it down into smaller subproblems and solving them recursively using functions f and g
 // Example: Cutting larger problems into smaller ones (LC 70. Climbing Stairs)
 
-
-
 // Memoization (Top-Down) approach using unordered_map
 int fibMemo(int n, std::unordered_map<int, int>& memo) {
     if (n <= 1) {
@@ -20,8 +18,6 @@ int fibMemo(int n, std::unordered_map<int, int>& memo) {
     return memo[n];
 }
 
-
-
 // Tabulation (Bottom-Up) approach using vector
 int fibTab(int n) {
     std::vector<int> dp(n + 1, 0);
@@ -34,3 +30,13 @@ int fibTab(int n) {
 
     return dp[n];
 }
+
+// Notes: The fibMemo function represents the memoization approach. 
+// It uses an unordered_map called memo to store the previously computed Fibonacci numbers, avoiding redundant calculations. 
+// The function recursively calculates the Fibonacci number for a given index n while utilizing the stored values in the memo map.
+// The fibTab function represents the tabulation approach. It uses a vector dp to store the Fibonacci numbers in a bottom-up manner. 
+// Starting from the base cases dp[0] = 0 and dp[1] = 1, the function iteratively calculates the Fibonacci numbers up to the target index n.
+// Both approaches yield the same Fibonacci number for the given index n. 
+// You can modify the n variable in the main function to compute Fibonacci numbers for different indices.
+// This template can be generalized and adapted for other dynamic programming problems,
+// where memoization and tabulation techniques are often used for optimizing recursive solutions and reducing time complexity.
