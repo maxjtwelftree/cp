5eabbff21fc7b65aee297b1ac58432880bf9eeaa
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-07-22 18:16:26 +1000

    Create binary-search.cpp

---
+++ templates/binary-search.cpp
@@ -0,0 +1,22 @@
+// Title: Binary Search Function
+// Parameters: arr, target
+// Returns: Index of the target element if it exists in the array, -1 otherwise
+// Description: Performs a binary search on a sorted array to find a given target element
+// Example: Finding a word in a dictionary
+int binarySearch(vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; // Target found at index mid
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+    return -1; // Target not found
+}
