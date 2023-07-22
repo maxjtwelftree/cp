23574b2d42893d16bce8346359ecef493268aa2a
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-07-22 21:38:06 +1000

    Create depth-first-search.cpp

---
+++ templates/depth-first-search.cpp
@@ -0,0 +1,30 @@
+// Title: Depth First Search Function 
+// Parameters: graph, start
+// Returns: A list of all vertices in the graph that can be reached from the starting vertex start
+// Description: Performs a depth-first search on a graph starting from the given vertex start
+
+// Definition for a tree node
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// DFS function for graphs (adjacency list representation)
+// If working with a binary tree, replace `vector<vector<int>>` with the appropriate tree representation
+void dfs(vector<vector<int>>& graph, int node, vector<bool>& visited) {
+    // Mark the current node as visited
+    visited[node] = true;
+
+    // Process the current node
+    // For LeetCode problems, you may perform the necessary operations here
+
+    // Traverse all neighbors of the current node
+    for (int neighbor : graph[node]) {
+        // If the neighbor is not visited, recursively call the DFS function
+        if (!visited[neighbor]) {
+            dfs(graph, neighbor, visited);
+        }
+    }
+}
