868bc0aab9d9d5eed3781d73ade6d02cfa945c72
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-07-22 21:36:30 +1000

    Update binary-search.cpp

--- templates/binary-search.cpp
+++ templates/binary-search.cpp
@@ -3,6 +3,7 @@
 // Returns: Index of the target element if it exists in the array, -1 otherwise
 // Description: Performs a binary search on a sorted array to find a given target element
 // Example: Finding a word in a dictionary
+
 int binarySearch(vector<int>& arr, int target) {
     int left = 0;
     int right = arr.size() - 1;
