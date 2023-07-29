f43d47c81dab1ef6495ee8a3ff2045f32ddeb9bc
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-07-29 20:35:50 +0930

    Time: 88 ms (99.89%) | Memory: 93.2 MB (99.18%) - LeetSync

---
+++ leetcode/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i = 0;
+        int j;
+        int max_profit = 0;
+
+        for (j = 1; j < prices.size(); j++) {
+            if (prices[j] - prices[i] > 0) max_profit = max(max_profit, prices[j]-prices[i]);
+            else
+                i = j;
+        }
+        return max_profit;
+    }
+};
