a4373b8bde2e1072d4a2fe7050c15bcab1b71995
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-02 17:01:03 +0930

    Time: 19 ms (93.81%) | Memory: 23.9 MB (76.88%) - LeetSync

--- leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
+++ leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         vector<int> result(n, 1);
 
-        int prefix_product = 1;
-        for (int i = 0; i < n; i++) {
-            result[i] *= prefix_product;
-            prefix_product *= nums[i];
+        int prefix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= prefix;
+            prefix *= nums[i];
         }
 
-        int suffix_product = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] *= suffix_product;
-            suffix_product *= nums[i];
+        int suffix = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] *= suffix;
+            suffix *= nums[i];
         }
 
         return result;
