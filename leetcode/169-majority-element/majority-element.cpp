de37bc1e9755505afb7c35693aaf1e1807e9e518
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-05 16:05:42 +0930

    Time: 17 ms (78.88%) | Memory: 19.7 MB (13.21%) - LeetSync

--- leetcode/169-majority-element/majority-element.cpp
+++ leetcode/169-majority-element/majority-element.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        int majority = nums[0], votes = 1;
-
-        for (int i = 1; i < n; i++) {
-            if (votes == 0) {
-                votes++;
-                majority = nums[i];
-            }
-            else if (majority == nums[i]) {
-                votes++;
-            }
-            else {
-                votes--;
-            }
-        }
-        return majority;
+        return nums[n/2];
     }
 };
