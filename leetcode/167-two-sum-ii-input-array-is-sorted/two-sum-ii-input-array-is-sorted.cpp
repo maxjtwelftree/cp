fbccb17fc08f6d8de1b7f50d1db1efaf7c35b37a
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-06 13:27:47 +0930

    Time: 11 ms (83.04%) | Memory: 15.6 MB (27.38%) - LeetSync

--- leetcode/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ leetcode/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int n=numbers.size();
+        int n = numbers.size();
 
-        for (int i = 0, j = n-1; i < j;) {
+        for (int i = 0, j = n - 1; i < j;) {
             int sum = numbers[i] + numbers[j];
-            if (sum == target) return {i+1, j+1};            
-            else if ( sum< target) i++;            
+            if (sum == target) return {i + 1, j + 1};
+            else if (sum < target) i++;
             else j--;
         }
-        return {};        
-    }   
+        return {};
+    }
 };
