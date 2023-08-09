6924c4f1bb9cc3167ed59c231c94e36234280b9b
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-09 23:00:31 +0930

    Time: 0 ms (100.00%) | Memory: 6.8 MB (11.20%) - LeetSync

---
+++ leetcode/409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> map;
+        int oddCount = 0;
+        for (char ch : s) {
+            map[ch]++;
+            if (map[ch] % 2 == 1) {
+                oddCount++;
+            }
+            else {
+                oddCount--;
+            }
+        }
+        if (oddCount > 1) {
+            return s.length() - oddCount + 1;
+        }
+        return s.length();
+    }
+};
