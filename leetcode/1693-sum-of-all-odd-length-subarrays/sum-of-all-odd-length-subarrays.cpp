708ee0fdfa54c126568d18c4197635ef04b37f51
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-09 14:53:59 +0930

    Time: 0 ms (100.00%) | Memory: 8.2 MB (95.32%) - LeetSync

--- leetcode/1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
+++ leetcode/1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         int n = arr.size();
-        int result = 0; 
-        // start with result and n to make code cleaner 
+        int result = 0;
+
         for (int i = 0; i < n; i++) {
-            int start = n - i;
-            int end = i + 1;
-            int total = start * end;
-            int odd = total/2;
-            // give values for notes 
-            if (total % 2 == 1) {
-                odd++;
-            }
-            // check if total equals an odd 
-            result += odd * arr[i];
-            // give result value, += is equivalent to result = result + (odd * arr[i])
+            int start = i + 1;
+            int end = n - i;
+            result += ((start) * (end) + 1) / 2 * arr[i];
         }
         return result;
     }
