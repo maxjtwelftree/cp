0deca9ef7a588325d17711d05c4233f38ef015aa
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-11 20:53:22 +0930

    Time: 10 ms (89.18%) | Memory: 9.1 MB (5.07%) - LeetSync

--- leetcode/383-ransom-note/ransom-note.cpp
+++ leetcode/383-ransom-note/ransom-note.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> m;
-        for (auto c: magazine) {
-            m[c]++;
+        unordered_map<char, int> map1, map2;
+
+        for (char ch : ransomNote) {
+            map1[ch]++;
         }
-        for (auto c: ransomNote) {
-            if (m.find(c) == m.end() || m[c] <= 0) {
+
+        for (char ch : magazine) {
+            map2[ch]++;
+        }
+
+        for (auto entry : map1) {
+            if (map2[entry.first] < entry.second) {
                 return false;
             }
-            m[c]--;
         }
+
         return true;
     }
 };
