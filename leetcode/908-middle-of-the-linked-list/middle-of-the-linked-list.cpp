bf11079e2a1369721d7bfcb5d6f173fd991c2fc7
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-15 21:46:24 +0930

    Time: 0 ms (100.00%) | Memory: 7.1 MB (8.78%) - LeetSync

--- leetcode/908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ leetcode/908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -1,30 +1,13 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        // we need to check if there is even a node at head
-        // if not a NULL dosent have a next so it would errors
         if (head == NULL) {
             return 0;
         }
-        // create variable to track nodes we have
         int count = 0;
-
-        // iterator pointer
         ListNode* temp = head;
         while (temp != NULL) {
-            // incrementing count bc we have seen value
             count++;
-            // move to next node
             temp = temp->next;
         }
         int mid = count/2;
