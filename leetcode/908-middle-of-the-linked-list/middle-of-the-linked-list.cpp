3089070b12c09a8bc8b828780542dc71680c920d
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-15 21:46:35 +0930

    Time: 0 ms (100.00%) | Memory: 7 MB (42.97%) - LeetSync

--- leetcode/908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ leetcode/908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) {
-        if (head == NULL) {
-            return 0;
-        }
-        int count = 0;
-        ListNode* temp = head;
-        while (temp != NULL) {
-            count++;
-            temp = temp->next;
-        }
-        int mid = count/2;
-        temp = head;
-        for (int i = 0; i < mid; i++) {
-            temp = temp->next;
-        }
-        return temp;
+        ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        return slow;
     }
 };
