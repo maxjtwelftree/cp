c2da2b5acaea6c4ca203a1f72fa81a028a30096f
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-16 16:03:50 +0930

    Time: 3 ms (78.42%) | Memory: 10.6 MB (89.01%) - LeetSync

---
+++ leetcode/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *fast = head, *slow = head;
+        for (int i = 0; i < n; i++) {
+		    fast = fast->next;
+	    }
+	    if (fast == nullptr) {
+            return head->next;
+        }
+        while (fast->next) {
+		    slow = slow->next;
+		    fast = fast->next;
+	    }
+	    slow->next = slow->next->next;
+	    return head;
+    }
+};
