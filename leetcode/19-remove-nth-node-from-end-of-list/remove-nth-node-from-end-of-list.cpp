22ec1d9db07980bc3580d9b10142dbd9c102004b
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-16 16:09:26 +0930

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.59%) - LeetSync

--- leetcode/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ leetcode/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode *fast = head, *slow = head;
-        for (int i = 0; i < n; i++) {
-		    fast = fast->next;
-	    }
-	    if (fast == nullptr) {
-            return head->next;
+    ListNode *removeNthFromEnd(ListNode *head, int &n) {
+        if (head == nullptr) {
+            return nullptr;
         }
-        while (fast->next) {
-		    slow = slow->next;
-		    fast = fast->next;
-	    }
-	    slow->next = slow->next->next;
-	    return head;
+
+        int count = 0;
+        ListNode* current = head;
+        while (current != nullptr) {
+            ++count;
+            current = current->next;
+        }
+
+        int deleted = count - n + 1;
+
+        if (deleted <= 0) {
+            // Invalid 'n' value, return the original list.
+            return head;
+        }
+
+        if (deleted == 1) {
+            // If the first ListNode is to be deleted, update the head and return.
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+            return head;
+        }
+
+        ListNode* del_prev = head;
+        for (int i = 1; i < deleted - 1 && del_prev != nullptr; ++i) {
+            del_prev = del_prev->next;
+        }
+
+        if (del_prev == nullptr || del_prev->next == nullptr) {
+            // 'n' is out of bounds, return the original list.
+            return head;
+        }
+
+        ListNode* del = del_prev->next;
+        del_prev->next = del->next;
+        delete del;
+
+        return head;
     }
 };
