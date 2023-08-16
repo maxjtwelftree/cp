16e43f54f84d968b68730b9b7c23e24fb6f03413
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-16 16:17:06 +0930

    Time: 7 ms (32.13%) | Memory: 10.8 MB (12.97%) - LeetSync

--- leetcode/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ leetcode/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
-    ListNode *removeNthFromEnd(ListNode *head, int &n) {
-        if (head == nullptr) {
-            return nullptr;
-        }
-
-        int count = 0;
-        ListNode* current = head;
-        while (current != nullptr) {
-            ++count;
-            current = current->next;
-        }
-
-        int deleted = count - n + 1;
-
-        if (deleted <= 0) {
-            // Invalid 'n' value, return the original list.
-            return head;
-        }
-
-        if (deleted == 1) {
-            // If the first ListNode is to be deleted, update the head and return.
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
-            return head;
-        }
-
-        ListNode* del_prev = head;
-        for (int i = 1; i < deleted - 1 && del_prev != nullptr; ++i) {
-            del_prev = del_prev->next;
-        }
-
-        if (del_prev == nullptr || del_prev->next == nullptr) {
-            // 'n' is out of bounds, return the original list.
-            return head;
-        }
-
-        ListNode* del = del_prev->next;
-        del_prev->next = del->next;
-        delete del;
-
-        return head;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+	ListNode* temp = head;
+	int length = 0, i = 1;
+	while (temp) {
+        // finding the length of linked list
+        temp = temp -> next;
+        length++;
+    }
+	if (length == n) {
+        // if head itself is to be deleted, just return head 
+        return head -> next;  
+    }        
+	for (temp = head; i < length - n; i++) {
+        // tempate first len-n nodes
+        temp = temp -> next; 
+    }
+    // remove the nth node from the end
+    temp -> next = temp -> next -> next;     
+	return head;
     }
 };
