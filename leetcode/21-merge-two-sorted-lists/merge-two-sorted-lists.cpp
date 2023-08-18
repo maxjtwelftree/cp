155d0f3a3e24dbe04cba0b89f86cf61aae975a31
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-18 15:40:42 +0930

    Time: 4 ms (83.98%) | Memory: 14.9 MB (24.23%) - LeetSync

--- leetcode/21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ leetcode/21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(0);        
-        ListNode* curr = &dummy;
 
-        while (list1 != NULL && list2 != NULL) {
-            if (list1->val < list2->val) {
-                curr->next = list1;
-                list1 = list1->next;
-            }
-            else {
-                curr->next = list2;
-                list2 = list2->next;
-            }
-            curr = curr->next;
+        if (list1 == NULL) {
+            return list2;
         }
-        if (list1 != NULL) {
-            curr->next = list1;
-        } else {
-            curr->next = list2;
+        if (list2 == NULL) {
+            return list1;
         }
-        return dummy.next;  
+        while (list1->val <= list2->val) {
+            list1 -> next = mergeTwoLists (list1-> next, list2);
+            return list1;
+        }
+        while (list2->val <= list1->val) {
+            list2 -> next = mergeTwoLists (list2-> next, list1);
+            return list2;
+        }
+        return list1;
     }
 };
