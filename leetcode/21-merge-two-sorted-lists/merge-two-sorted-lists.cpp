2aead419aecc3917eeea23409337cf218bcf7ffe
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-18 15:40:22 +0930

    Time: 11 ms (28.43%) | Memory: 14.7 MB (58.37%) - LeetSync

--- leetcode/21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ leetcode/21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,31 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);        
+        ListNode* curr = &dummy;
 
-        if (list1 == NULL) {
-            return list2;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
         }
-        if (list2 == NULL) {
-            return list1;
+        if (list1 != NULL) {
+            curr->next = list1;
+        } else {
+            curr->next = list2;
         }
-        while (list1->val <= list2->val) {
-            list1 -> next = mergeTwoLists (list1-> next, list2);
-            return list1;
-        }
-        while (list2->val <= list1->val) {
-            list2 -> next = mergeTwoLists (list2-> next, list1);
-            return list2;
-        }
-        return list1;
+        return dummy.next;  
     }
 };
