fc862496ac40fe6f9f4a321d604b9ad903dfa11a
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-20 15:53:04 +0930

    Time: 29 ms (91.96%) | Memory: 17.8 MB (57.30%) - LeetSync

--- leetcode/143-reorder-list/reorder-list.cpp
+++ leetcode/143-reorder-list/reorder-list.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if(!head || !head->next || !head->next->next) {
-            return;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next != NULL) {
+            fast = fast->next->next;
+            slow=slow->next;
         }
-        ListNode* pointed = head;
-        while (pointed->next->next != NULL) {
-            pointed = pointed->next;
+        ListNode *head2=slow->next;
+        slow->next = NULL;   
+
+        ListNode* forward = NULL;
+        ListNode* prev = NULL;
+        ListNode* curr = head2;
+
+        while (curr) {
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
         }
-        pointed->next->next = head->next;
-        head->next = pointed->next;
-        pointed->next = NULL;
-        reorderList(head->next->next);
+
+        head2 = prev;
+        while (head2) {
+            ListNode *ptr1=head->next;
+            ListNode *ptr2=head2->next;
+            
+            head->next=head2;
+            head2->next=ptr1;
+            head=ptr1;
+            head2=ptr2;
+        }
     }
 };
