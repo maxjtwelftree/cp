ef1c52476db21fc54d7a907792e52cc69d612ae6
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-21 10:16:36 +0930

    Time: 11 ms (47.94%) | Memory: 11 MB (36.89%) - LeetSync

--- leetcode/674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.cpp
+++ leetcode/674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int findLengthOfLCIS(vector<int>& nums) {
-        int n = nums.size();
         int count = 1;
-        int result = 1;
+        int maxCount = 1;
+        int n = nums.size();
+
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] < nums[i+1]) {
                 count++;
-                result = max(result, count);
+                maxCount = max(maxCount, count);
             }
             else {
                 count = 1;
             }
         }
-        return result;
+        return maxCount;
     }
 };
