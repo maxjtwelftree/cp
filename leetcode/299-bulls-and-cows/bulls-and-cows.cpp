3094227a9432b2ae05e53d8b6f4da48319f364c8
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-22 19:35:07 +0930

    Time: 5 ms (50.25%) | Memory: 6.6 MB (33.52%) - LeetSync

---
+++ leetcode/299-bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        vector<int> freq(10,0);
+        string hint;
+        int bulls=0,cows=0;
+
+        for(int i=0;i<secret.size();++i){
+            if(guess[i]==secret[i]){
+                bulls++;
+            }else {
+                if(freq[secret[i]-'0']++ < 0) cows++;
+                if(freq[guess[i]-'0']-- > 0) cows++;
+            }
+        }
+        hint=to_string(bulls)+"A"+to_string(cows)+"B";
+        return hint;
+    }
+};
