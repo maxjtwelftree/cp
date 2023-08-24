21e60b4835a6e7b49df42cc9cbfc773b8cd2184d
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-24 16:43:25 +0930

    Time: 7 ms (79.38%) | Memory: 7.4 MB (17.08%) - LeetSync

--- leetcode/242-valid-anagram/valid-anagram.cpp
+++ leetcode/242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort (s.begin(), s.end());
-        sort (t.begin(), t.end());
-        if (s != t) {
-            return false;
+        unordered_map<char, int> map1, map2;
+
+        for (char ch : s) {
+            map1[ch]++;
         }
-        return true;
+        for (char ch : t) {
+            map2[ch]++;
+        }
+        if (map1 == map2) {
+            return true;
+        }
+        return false;
     }
 };
