7fa3008a5d771b1ce891fc1112970f2633146657
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-08-30 15:53:15 +0930

    Time: 21 ms (59.77%) | Memory: 24 MB (78.89%) - LeetSync

--- leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
+++ leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -7,15 +7,13 @@ public:
         int prefix = 1;
         for (int i = n - 1; i >= 0; i--) {
             result[i] *= prefix;
-            prefix *= nums[i];
+            prefix *= nums[i];        
         }
-
         int suffix = 1;
         for (int i = 0; i < n; i++) {
             result[i] *= suffix;
-            suffix *= nums[i];
+            suffix *= nums[i];        
         }
-
         return result;
     }
 };
