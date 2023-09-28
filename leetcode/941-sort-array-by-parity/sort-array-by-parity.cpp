d500e46004d405b9afab08d771d9bceb80e57964
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-09-28 14:11:07 +0930

    Time: 16 ms (6.91%) | Memory: 16.9 MB (8.80%) - LeetSync

--- leetcode/941-sort-array-by-parity/sort-array-by-parity.cpp
+++ leetcode/941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -4,12 +4,14 @@ public:
         vector<int> res;
         int n = nums.size();
 
+        // pushing back to the left
         for (int i = 0; i < n; i++) {
             if (nums[i] % 2 == 0) {
                 res.push_back(nums[i]);
             }
         } 
         
+        // pushing back to the right
         for (int i = 0; i < n; i++) {
             if (nums[i] % 2 == 1) {
                 res.push_back(nums[i]);
