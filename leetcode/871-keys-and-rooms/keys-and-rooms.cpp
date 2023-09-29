ddb9f49ff7d9d1e4c9a663bf402121feeb6a9d8e
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-09-29 10:45:53 +0930

    Time: 11 ms (27.20%) | Memory: 10.9 MB (23.13%) - LeetSync

--- leetcode/871-keys-and-rooms/keys-and-rooms.cpp
+++ leetcode/871-keys-and-rooms/keys-and-rooms.cpp
@@ -12,11 +12,9 @@ public:
 
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
-
         vector<int> vis(n);
 
         dfs(0, vis, rooms);
-        
 
         for (auto u : vis) {
             if (u == 0) {
@@ -25,17 +23,5 @@ public:
         }
 
         return true;
-
-        
-
-
-        // DFS
-        // where with each room (node) we visit we collect that nums[i] key, and then if we have key = room as we DFS we can continue, if we dont we cannot
-
-        // we know we are at true if we have searched all rooms otherwise we return false
-
-
- 
-        
     }
 };
