2bfbdfbea56ddb3653fa9a035bfe430a5cf50774
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-10-14 21:22:00 +1030

    Time: 62 ms (86.22%) | Memory: 59.2 MB (42.76%) - LeetSync

--- leetcode/11-container-with-most-water/container-with-most-water.cpp
+++ leetcode/11-container-with-most-water/container-with-most-water.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        int n = height.size();
         int i = 0;
-        int j = height.size() - 1;
-        int curr = 0;        
+        int j = n - 1;
+        int current = 0;
         int result = 0;
 
         while (i < j) {
-            curr = (j - i) * min(height[i], height[j]);
-            result = max(result, curr);
+            current = (j - i) * min(height[i], height[j]);
+            result = max(result, current);
 
             if (height[i] <= height[j]) {
                 i++;
-            }
-            else {
-            j--;
+            } else{
+                j--;
             }
         }
         return result;
