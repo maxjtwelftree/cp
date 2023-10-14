2f81e3a2535b1b3a650a05226465adaf3a8fb1c3
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-10-14 21:48:38 +1030

    Time: 3 ms (58.75%) | Memory: 10.4 MB (41.81%) - LeetSync

--- leetcode/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ leetcode/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int x = nums[0];
-        for (int i = 1; i <= nums.size(); i++) {
-            if (nums[i-1] < x) x = nums[i-1];
-        }
-        return x;
+        return *min_element(nums.begin(), nums.end());
     }
 };
