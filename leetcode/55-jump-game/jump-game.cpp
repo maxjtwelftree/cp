06c00cfc0f3013ceb882c8edcdb94bfdd309160f
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-10-17 11:41:13 +1030

    Time: 40 ms (89.74%) | Memory: 48.9 MB (21.24%) - LeetSync

--- leetcode/55-jump-game/jump-game.cpp
+++ leetcode/55-jump-game/jump-game.cpp
@@ -3,14 +3,16 @@ public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
         int next = 0;
-
+        // greedy very greedy
         for (int i = 0; i < n; i++) {
             if (i > next) {
                 return false;
-            }
+            } 
+            // this check is key
             next = max(next, nums[i] + i);
         }
+
+        return true;
         
-        return true;        
     }
 };
