9de199c0d836443a1630910c4357ff1e1029a71c
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-10-20 11:53:35 +1030

    Time: 1100 ms (24.18%) | Memory: 192 MB (9.81%) - LeetSync

--- leetcode/15-3sum/3sum.cpp
+++ leetcode/15-3sum/3sum.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        set<vector<int> >s;
+        vector<vector<int>> triples_vector;
+        set<vector<int>> my_set;
         int n = nums.size();
-        sort (nums.begin(), nums.end());
-        for(int i = 0; i < n; i++) {
-                int j = i + 1;
-                int k = n - 1;
-                while (j < k) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if (sum == 0) {
-                        s.insert ({nums[i], nums[j], nums[k]});
-                    }
-                    if(sum > 0){
-                        k--;
-                    }
-                    else {
-                        j++;
-                    }
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n-2; i++) {
+            // if (i > 0) break;
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    my_set.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
                 }
-            
+            }
         }
-        for (auto i : s) {
-            ans.push_back(i);
+
+        for (auto ans : my_set) {
+            triples_vector.push_back(ans);
         }
-        return ans;
+
+        return triples_vector;
     }
-};
+}; 
