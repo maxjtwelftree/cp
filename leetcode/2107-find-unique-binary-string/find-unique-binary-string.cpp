93c74fde44b7c5f5a1d94f83adb70ce3f35915c0
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-16 13:56:47 +1030

    Time: 0 ms (100.00%) | Memory: 10.4 MB (86.71%) - LeetSync

--- leetcode/2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ leetcode/2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    void dfs() {
-
-    }
-
     string findDifferentBinaryString(vector<string>& nums) {
         int n = nums.size();
         string ans = "";
         
         for (int i = 0; i < n; i++) {
             char current_check = nums[i][i];
-            // 01, 10 
             ans += current_check == '0' ? '1' : '0';
-
-            // 1, 10, 
         }
 
         return ans;
-
-
-        // ? <- "01" -> "10" == 1 -> 10
-        // <- "111" -> "011" ->"001"
-        
     }
 };
