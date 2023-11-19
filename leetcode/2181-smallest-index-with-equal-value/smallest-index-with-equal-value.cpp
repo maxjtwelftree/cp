104d9ff60a736e1aa8e30949d96a72eda02a1eeb
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-20 09:54:10 +1030

    Time: 7 ms (74.21%) | Memory: 22.6 MB (14.53%) - LeetSync

--- leetcode/2181-smallest-index-with-equal-value/smallest-index-with-equal-value.cpp
+++ leetcode/2181-smallest-index-with-equal-value/smallest-index-with-equal-value.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int smallestEqual(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         for (auto i = 0; i < nums.size(); i++) {
             if (i % 10 == nums[i]) return i;
         }
