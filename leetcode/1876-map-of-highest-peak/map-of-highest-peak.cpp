0bd42f638f06e4901398bf6b9e75f30c87df1312
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-21 15:56:36 +1030

    Time: 235 ms (91.81%) | Memory: 108.6 MB (71.67%) - LeetSync

--- leetcode/1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ leetcode/1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -4,17 +4,12 @@ public:
         int n = isWater.size(), m = isWater[0].size();
         queue<pair<int,int>> q;
 
-        // change our matrix so all water is 0 and then our land is -1 (unvisited)
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                // changes land cells (the only ones we need to visit)
                 if (isWater[i][j] == 0) {
                     isWater[i][j] = -1;
                 } else {
-                    // changes water cells
                     isWater[i][j] = 0;
-                    // push these to our queue as this is where we want to start our 
-                    // traversal
                     q.push({i, j});
                 }
             }
