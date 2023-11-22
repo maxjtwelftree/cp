d81463d013de484cac4af58539b76e6803357d2e
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-22 19:07:14 +1030

    Time: 10 ms (35.67%) | Memory: 13.8 MB (74.67%) - LeetSync

--- leetcode/280-wiggle-sort/wiggle-sort.cpp
+++ leetcode/280-wiggle-sort/wiggle-sort.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-
-        //[1,2,3,4,5,6]
-        // 
+        
         for (int i = 1; i < nums.size() - 1; i+=2) {
             swap(nums[i], nums[i+1]);
         }
