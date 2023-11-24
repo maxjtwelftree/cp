5065df0931f7c8b5d67fab03ba66ff5ad38900db
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-24 20:24:01 +1030

    Time: 143 ms (88.27%) | Memory: 17.8 MB (36.54%) - LeetSync

--- leetcode/169-majority-element/majority-element.py
+++ leetcode/169-majority-element/majority-element.py
@@ -1,5 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        voting_count = 0
+        current_candidate = None
+
+        for num in nums:
+            if voting_count == 0:
+                current_candidate = num
+            voting_count += (1 if num == current_candidate else -1)
+        
+        return current_candidate
         
