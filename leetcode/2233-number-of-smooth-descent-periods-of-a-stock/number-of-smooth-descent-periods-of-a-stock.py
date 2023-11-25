da71ee356631d923349a5f96edbff4ac4f26bd2f
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-25 17:43:24 +1030

    Time: 908 ms (85.81%) | Memory: 30.3 MB (12.87%) - LeetSync

--- leetcode/2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.py
+++ leetcode/2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.py
@@ -1,11 +1,9 @@
 class Solution:
     def getDescentPeriods(self, prices: List[int]) -> int:
-        dp=[0 for i in range(len(prices))] 
-        dp[0] = 1
+        dp = [1] * len(prices)
 
         for i in range(1,len(prices)):
-            if prices[i]+1 == prices[i-1]:
-                dp[i] += dp[i-1]
-            dp[i] += 1
-
+            if prices[i] + 1 == prices[i-1]:
+                dp[i] = dp[i-1] + 1
+        
         return sum(dp)
