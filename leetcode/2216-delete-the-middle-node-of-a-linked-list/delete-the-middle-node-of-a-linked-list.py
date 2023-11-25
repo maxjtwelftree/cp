0495f02476a586d9fdf2eedbda58e4f9438cceba
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-25 21:40:40 +1030

    Time: 1324 ms (22.47%) | Memory: 62.8 MB (88.34%) - LeetSync

--- leetcode/2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ leetcode/2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -1,21 +1,14 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0, head)
         slow = dummy 
         fast = dummy 
 
-        # we are literally just using t and h to find the middle and then calling delete on the slow node by assigning it to a nullptr 
 
         while fast.next and fast.next.next:
-            fast = fast.next.next # move this twice the speed
+            fast = fast.next.next
             slow = slow.next
 
-        # this will delete, due to pointing it to a null value 
         slow.next = slow.next.next
-        return dummy.next # else this will return a 0 before the list 
+        return dummy.next 
         
