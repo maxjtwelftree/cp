be924e56d7ff060d382132797055790cfbb8b1f7
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-25 13:50:36 +1030

    Time: 36 ms (85.18%) | Memory: 16.3 MB (52.79%) - LeetSync

--- leetcode/389-find-the-difference/find-the-difference.py
+++ leetcode/389-find-the-difference/find-the-difference.py
@@ -1,7 +1,8 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        return list(Counter(t) - Counter(s))[0]
-        # constructing counters is O(n)
+        for i in t:
+            if s.count(i) != t.count(i):
+                return i
 
         
         
