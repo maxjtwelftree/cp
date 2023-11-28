a9be629fa5c92cfa33bc8d6b5c1897a409985b9f
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-28 12:54:37 +1030

    Time: 305 ms (95.71%) | Memory: 17.5 MB (44.29%) - LeetSync

--- leetcode/2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.py
+++ leetcode/2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.py
@@ -2,6 +2,9 @@ class Solution:
     def numberOfWays(self, corridor: str) -> int:
         mod = 10**9 + 7
         a, b, c = 0, 0, 1 # a = 0 seats, b = 1 seat, c = 2 seats
+        # if seats == odd than answer == 0
+        # if seats == even then divider count
+        
 
         for checker in corridor:
             if checker == 'S':
