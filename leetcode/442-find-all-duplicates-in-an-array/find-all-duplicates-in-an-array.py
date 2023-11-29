fd0d78de5a8919c37bdcbc8ac532dd5a2024eec3
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-11-29 15:31:42 +1030

    Time: 283 ms (89.94%) | Memory: 25.3 MB (23.62%) - LeetSync

--- leetcode/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ leetcode/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,10 +1,7 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
         checker, res = Counter(nums), []
-        
         for i, j in checker.items():
             if j == 2:
                 res.append(i)
-        
         return res
-        
