b7dcd3b2ce34859d7a51369eaa2059ceb746fbb7
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-01 07:22:19 +1030

    Time: 28 ms (97.19%) | Memory: 16.4 MB (10.22%) - LeetSync

--- leetcode/2365-percentage-of-letter-in-string/percentage-of-letter-in-string.py
+++ leetcode/2365-percentage-of-letter-in-string/percentage-of-letter-in-string.py
@@ -1,3 +1,7 @@
 class Solution:
     def percentageLetter(self, s: str, letter: str) -> int:
-        return int((s.count(letter) / len(s)) * 100)
+        n = len(s)
+        (n.bit_length().bit_length())
+        print(n)
+        return round(math.floor((s.count(letter)/len(s))*100))     
+
