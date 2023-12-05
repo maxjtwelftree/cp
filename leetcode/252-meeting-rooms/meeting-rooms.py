eab52ef711b83a89aadf65ab293f0211496a3311
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-05 15:40:26 +1030

    Time: 87 ms (16.70%) | Memory: 19.7 MB (76.54%) - LeetSync

--- leetcode/252-meeting-rooms/meeting-rooms.py
+++ leetcode/252-meeting-rooms/meeting-rooms.py
@@ -1,13 +1,9 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         new_list = sorted(intervals)
-        print(new_list)
         for i in range(1, len(new_list)):
             if new_list[i-1][1] > new_list[i][0]:
-                print(new_list[i-1][1])
-                print(new_list[i][0])
                 return False
         
-        print(new_list)
         return True
         
