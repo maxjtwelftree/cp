ebd1b930ec0b9c43009ece7982f32171aa68822d
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-06 18:44:14 +1030

    Time: 919 ms (66.61%) | Memory: 30.3 MB (42.49%) - LeetSync

--- leetcode/2245-destroying-asteroids/destroying-asteroids.py
+++ leetcode/2245-destroying-asteroids/destroying-asteroids.py
@@ -1,7 +1,8 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
-        for x in sorted(asteroids):
-            if mass < x:
+        new_asteroids = sorted(asteroids)
+        for i in range(len(new_asteroids)):
+            if mass < new_asteroids[i]:
                 return False
-            mass += x
+            mass += new_asteroids[i]
         return True
