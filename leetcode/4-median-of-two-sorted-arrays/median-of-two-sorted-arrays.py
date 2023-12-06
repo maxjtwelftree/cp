2f0d783018e3480cf1057f12b16fb6a9bb9dc8a5
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-06 15:31:35 +1030

    Time: 96 ms (28.41%) | Memory: 16.7 MB (23.00%) - LeetSync

--- leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,4 +1,3 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:        
-        return sorted(nums1 + nums2)[len(nums1 + nums2) // 2] if len(nums1 + nums2) % 2 == 1 else \
-        (float(sorted(nums1 + nums2)[len(nums1 + nums2) // 2 - 1]) + float(sorted(nums1 + nums2)[len(nums1 + nums2) // 2])) / 2.0 
+        return median(nums1 + nums2)
