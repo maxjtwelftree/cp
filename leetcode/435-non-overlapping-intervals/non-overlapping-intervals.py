7c2bde613b147d331c82a9baf89f50df0e55f155
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-10 12:09:15 +1030

    Time: 1110 ms (89.57%) | Memory: 55.4 MB (5.37%) - LeetSync

--- leetcode/435-non-overlapping-intervals/non-overlapping-intervals.py
+++ leetcode/435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -1,10 +1,7 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key = lambda x: x[1])
-        print(intervals)
         cnt, k = 0, -inf
-        # [[1, 2], [2, 3], [3, 4], [1, 3]]
-        # [[1, 2], [1, 3], [2, 3], [3, 4]]
 
         for i, j in intervals:
             if i >= k:
