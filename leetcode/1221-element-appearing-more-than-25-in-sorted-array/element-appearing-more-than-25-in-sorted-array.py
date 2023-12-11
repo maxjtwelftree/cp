69cc52da9cbf6380642fd84ea2764d4d29c7c086
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-11 11:02:59 +1030

    Time: 89 ms (66.55%) | Memory: 18 MB (19.18%) - LeetSync

--- leetcode/1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
+++ leetcode/1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
@@ -1,8 +1,3 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        average = int(len(arr) / 4)
-        ans = Counter(arr).items()
-        for x, y in ans:
-            if y > average:
-                return x
-        return 0        
+        return mode(arr)
