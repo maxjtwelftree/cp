57dd0a85c99a0fdde1467f68c3d133ca84d43950
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-12 11:38:03 +1030

    Time: 54 ms (72.81%) | Memory: 16.3 MB (41.49%) - LeetSync

--- leetcode/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
+++ leetcode/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
@@ -1,5 +1,4 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         nums.sort(reverse=True)
-        ans = (nums[0]-1) * (nums[1]-1)
-        return ans   
+        return (nums[0] - 1) * (nums[1] - 1)
