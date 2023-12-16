e7a262ef654ebab231887dcc8a2a34fbe1f0deb7
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-16 22:24:11 +1030

    Time: 62 ms (24.96%) | Memory: 17 MB (18.39%) - LeetSync

--- leetcode/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.py
+++ leetcode/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.py
@@ -1,4 +1,17 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        return min(nums)
+        left, right = 0, len(nums)-1
+
+        while left < right:
+            mid = (left + right)//2
+            mins = min(nums)
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            elif nums[mid] == nums[right]:
+                right -= 1
+            else:
+                right = mid
+            
+
+        return nums[left]
         
