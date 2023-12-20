fb4d71bbdea68b460157fb441c252eb4af78acf2
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-20 11:13:02 +1030

    Time: 21 ms (9.32%) | Memory: 47.1 MB (29.77%) - LeetSync

--- leetcode/2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ leetcode/2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        auto checker_1 = INT_MAX, checker_2 = INT_MAX;
+        auto checker_1 = std::numeric_limits<int>::max();
+        auto checker_2 = std::numeric_limits<int>::max();
+
         for (auto x : prices) {
             if (x < checker_1) {
                 checker_2 = checker_1;
                 checker_1 = x;
             } else if (x < checker_2) checker_2 = x;
         }
-        if (checker_1 + checker_2 <= money) return money - (checker_1 + checker_2);
-        return money;
+
+        int total_cost = checker_1 + checker_2;
+        return (total_cost <= money) ? (money - total_cost) : money;
     }
 };
