0cb828f73b24b6f76819e2e0eb8b11f199be5352
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-20 10:56:58 +1030

    Time: 11 ms (73.75%) | Memory: 47.1 MB (29.77%) - LeetSync

--- leetcode/2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ leetcode/2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        int n = prices.size(), other = money;
-        sort(prices.begin(), prices.end());
+        auto n = prices.size();
+        auto other = money;
+        nth_element(prices.begin(), prices.begin(), prices.end());
         money -= prices[0], money -= prices[1];
         return money < 0 ? other : money;
     }
