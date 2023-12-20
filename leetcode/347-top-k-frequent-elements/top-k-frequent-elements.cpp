ac257f193d92ad6506aa582f706e67e51879cc1d
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-20 22:45:10 +1030

    Time: 7 ms (94.18%) | Memory: 14.3 MB (31.94%) - LeetSync

--- leetcode/347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ leetcode/347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -4,9 +4,8 @@ public:
         unordered_map<int, int> find_k_frequency;
         for (const auto& x : nums) find_k_frequency[x]++;
         vector<pair<int, int>> freqVector(find_k_frequency.begin(), find_k_frequency.end());
-        sort(freqVector.begin(), freqVector.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-            return a.second > b.second;
-        });
+        nth_element(freqVector.begin(), freqVector.begin() + k, freqVector.end(), [](const auto& a, const auto& b) { return a.second > b.second; });
+        sort(freqVector.begin(), freqVector.begin() + k, [](const auto& a, const auto& b) { return a.second > b.second; });
         vector<int> output;
         output.reserve(k);
         for (auto x = 0; x < k; ++x) output.push_back(freqVector[x].first);
