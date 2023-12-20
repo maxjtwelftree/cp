e738eec11c91c10e35cf6f423411d8a898b73d7d
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-20 10:55:04 +1030

    Time: 11 ms (73.75%) | Memory: 47.1 MB (29.77%) - LeetSync

--- leetcode/2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ leetcode/2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -3,8 +3,7 @@ public:
     int buyChoco(vector<int>& prices, int money) {
         int n = prices.size(), other = money;
         sort(prices.begin(), prices.end());
-        money -= prices[0];
-        money -= prices[1];
+        money -= prices[0], money -= prices[1];
         return money < 0 ? other : money;
     }
 };
