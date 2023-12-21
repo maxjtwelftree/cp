884bf39e4998d4c17bab0e0288aa7c567d150648
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-21 11:04:19 +1030

    Time: 242 ms (49.52%) | Memory: 66.8 MB (61.45%) - LeetSync

--- leetcode/1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
+++ leetcode/1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxWidthOfVerticalArea(vector<vector<int>>& points) {
-        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
+        ranges::sort(points, [](const auto& a, const auto& b) {
             return a[0] < b[0];
         });
         
         auto finder = 0;
-        for (auto i = 1; i < size(points); ++i) 
+        for (size_t i = 1; i < size(points); ++i) 
             finder = max(finder, points[i][0] - points[i-1][0]);
         
         return finder;
