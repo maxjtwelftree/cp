3a06985f612aac123d146936278fda10c7330f81
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-22 15:14:04 +1030

    Time: 0 ms (100.00%) | Memory: 6.6 MB (62.95%) - LeetSync

--- leetcode/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ leetcode/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int maxScore(string s) {
-        auto count_1 = count(s.begin(), s.end(), '1');
-        auto count_2 = 0, max_score = 0;
+        int32_t count_1 = count(s.begin(), s.end(), '1');
+        int32_t count_2{};
+        int32_t max_score{};
         
         for (size_t c = 0; c < s.size() - 1; ++c) {
             count_2 += s[c] == '0';
             count_1 -= s[c] == '1';
-            max_score = max(max_score, static_cast<int>((count_1 + count_2)));
+            max_score = max(max_score, count_1 + count_2);
         }
         
         return max_score;
