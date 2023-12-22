e67b39f2c9b572c02bb7043eb754c38a1e8bf536
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-22 11:33:29 +1030

    Time: 185 ms (6.31%) | Memory: 34.9 MB (57.78%) - LeetSync

--- leetcode/621-task-scheduler/task-scheduler.cpp
+++ leetcode/621-task-scheduler/task-scheduler.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<int32_t, int32_t> map;
-        auto cnt = 0;
-
-        for (const auto& i : tasks) {
-            map[i]++;
-            cnt = max(cnt, map[i]);
+        vector<int> freq(26, 0);
+        for (char task : tasks) {
+            freq[task - 'A']++;
         }
 
-        auto ans = (cnt-1) * (n+1);
-
-        for (const auto& i : map) 
-            if (i.second == cnt) ans++;
+        vector<int> last_used(26, -n - 1); // Initialize last used time
+        int time = 0;
         
- 
-        return max(static_cast<int>(tasks.size()), ans);
+        for (int processed = 0; processed < tasks.size(); ) {
+            int taskIndex = -1;
+            int maxFreq = 0;
+
+            for (int i = 0; i < 26; ++i) {
+                if (freq[i] > maxFreq && time - last_used[i] > n) {
+                    taskIndex = i;
+                    maxFreq = freq[i];
+                }
+            }
+
+            if (taskIndex != -1) {
+                freq[taskIndex]--;
+                last_used[taskIndex] = time;
+                processed++;
+            }
+
+            time++;
+        }
+
+        return time;
     }
 };
+  
