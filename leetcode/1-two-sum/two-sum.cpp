da1373982b3290328253ffa510dfcfcc58923fae
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-28 23:15:35 +1030

    Time: 17 ms (50.75%) | Memory: 11.3 MB (34.18%) - LeetSync

--- leetcode/1-two-sum/two-sum.cpp
+++ leetcode/1-two-sum/two-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    [[nodiscard]] std::vector<int32_t> twoSum(std::vector<int32_t>& input, int32_t target) {
-        std::unordered_map<int32_t, int32_t> mapping;
-        for (size_t index = 0; index < input.size(); ++index) {
-            auto new_type = target - input[index];
-            if (mapping.count(new_type)) {
-                return {mapping[new_type], static_cast<int32_t>(index)};
+    vector<int> twoSum(vector<int>& input, int target) {
+        unordered_map<int32_t, int32_t> mapping{};
+        for (int32_t index = 0; index < input.size(); ++index) {
+            if (mapping.count(target - input[index])) {
+                return vector<int>{index, mapping[target - input[index]]};
             }
             mapping[input[index]] = index;
         }
 
-        return {};
+        throw invalid_argument{"Bruh"};
     }  
 };
