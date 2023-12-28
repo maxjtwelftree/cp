56d2a7573279f397180963668318a05bd59a97f4
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-28 23:15:51 +1030

    Time: 8 ms (78.89%) | Memory: 11.2 MB (34.18%) - LeetSync

--- leetcode/1-two-sum/two-sum.cpp
+++ leetcode/1-two-sum/two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& input, int target) {
-        unordered_map<int32_t, int32_t> mapping{};
+        unordered_map<int32_t, int32_t> mapping;
         for (int32_t index = 0; index < input.size(); ++index) {
             if (mapping.count(target - input[index])) {
                 return vector<int>{index, mapping[target - input[index]]};
