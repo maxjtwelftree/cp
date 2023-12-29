a3bbb506670c351a9c50f2f2d1842fafd7346d34
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-29 12:40:54 +1030

    Time: 92 ms (25.06%) | Memory: 7.9 MB (66.51%) - LeetSync

---
+++ leetcode/1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+class Solution {
+public:
+    int minDifficulty(std::vector<int>& jobDifficulty, int d) {
+        int n = jobDifficulty.size();
+        if (d > n) return -1;
+
+        const int32_t max = std::numeric_limits<int32_t>::max();
+        std::vector<int32_t> dp(n + 1, max);
+        dp[n] = 0;
+
+        for (int32_t day = 1; day <= d; ++day) {
+            std::vector<int32_t> next_dp(n + 1, max);
+            for (int32_t i = 0; i <= n - day; ++i) {
+                int32_t maxDiff = 0;
+                for (int j = i; j < n - day + 1; ++j) {
+                    maxDiff = std::max(maxDiff, jobDifficulty[j]);
+                    if (dp[j + 1] != max) { 
+                        next_dp[i] = std::min(next_dp[i], maxDiff + dp[j + 1]);
+                    }
+                }
+            }
+            dp = next_dp;
+        }
+
+        return dp[0] == max ? -1 : dp[0];
+    }
+};
