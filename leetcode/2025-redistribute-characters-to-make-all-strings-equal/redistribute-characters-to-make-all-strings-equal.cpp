c70a46ea3f82b0b0782add59d0d33e0de0085811
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-30 21:24:28 +1030

    Time: 37 ms (5.06%) | Memory: 12 MB (81.71%) - LeetSync

--- leetcode/2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ leetcode/2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     bool makeEqual(vector<string>& words) {
         unordered_map<char, int> mapping{};
-        size_t length = words.size();
+        const size_t length = words.size();
 
         for (const auto& word : words) 
-            for (char c : word) mapping[c]++;
+            for (const char& c : word) mapping[c]++;
 
-        for (auto x: mapping)
+        for (const auto& x: mapping) 
             if (x.second % length != 0) return false;
         return true;
-        
     }
 };
