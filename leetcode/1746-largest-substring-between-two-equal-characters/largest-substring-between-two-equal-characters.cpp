261c15f1ce218cc8a47bf81f77c1109b43fe04de
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-12-31 12:25:15 +1030

    Time: 0 ms (100.00%) | Memory: 6.7 MB (41.15%) - LeetSync

--- leetcode/1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ leetcode/1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        unordered_map<char, int> mapping{};
+        unordered_map<char, int> mapping_chars{};
         int max_length{-1};
 
         for (size_t i = 0; i < s.size(); ++i) { 
-            if (mapping.find(s[i]) == mapping.end()) mapping[s[i]] = i;
+            if (mapping_chars.find(s[i]) == mapping_chars.end()) mapping_chars[s[i]] = i;
             else {
-                max_length = std::max(max_length, static_cast<int>(i) - static_cast<int>(mapping[s[i]]) - 1);
+                max_length = max(max_length, static_cast<int>(i) - static_cast<int>(mapping_chars[s[i]]) - 1);
             }
         }
         return max_length;    
