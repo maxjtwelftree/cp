587352fedef608707dff5ddfd489f0a5c213b5a6
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-01 15:02:43 +1030

    Time: 22 ms (63.94%) | Memory: 17.7 MB (75.56%) - LeetSync

--- leetcode/455-assign-cookies/assign-cookies.cpp
+++ leetcode/455-assign-cookies/assign-cookies.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        ranges::sort(g), ranges::sort(s);
+        ranges::sort(g);
+        ranges::sort(s);
         size_t i{0};
 
-        for (size_t j = 0; i < g.size() && j < s.size(); j++) {
+        for (size_t j = 0; i < g.size() && j < s.size(); j++) 
             if (g[i] <= s[j]) i++;
-        }
+        
 
-        return i;
+        return static_cast<int>(i); // casted to int if needed for the return type
     }
 };
