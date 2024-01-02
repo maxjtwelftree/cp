8b719fa9ffc255e83c440766cbd61a49b3aa23fc
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-02 12:12:25 +1030

    Time: 23 ms (9.84%) | Memory: 30.2 MB (45.69%) - LeetSync

--- leetcode/2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ leetcode/2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        // get freqs;
-        auto mapping = std::unordered_map<int, int>{};
-        // [1,3,4,1,2,3,1]
-        // 1 = 3, 3 = 2, 2 = 1, 4 = 1
-
-        // then sort based on freqs with custom sort function
-        // 1 = 3, 3 = 2, 2 = 1, 4 = 1
-        // then iterate over these elements pushing them into a 2d vector on each iteration
-        // decrementing the frequencies as we go
-
+        auto mapping = unordered_map<int, int>{};
         for (const auto& x : nums) mapping[x]++;
 
         auto custom_sort_vect = vector<pair<int, int>>(mapping.begin(), mapping.end());
-        std::sort(custom_sort_vect.begin(), custom_sort_vect.end(),
-              [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
-                  return a.second > b.second;
-              });
+        std::ranges::sort(custom_sort_vect.begin(), custom_sort_vect.end(),
+            [](const auto& a, const auto& b) {
+                return a.second > b.second;
+            });
 
         vector<vector<int>> output_vector;
         for (auto it = custom_sort_vect.begin(); it != custom_sort_vect.end(); ++it) {
