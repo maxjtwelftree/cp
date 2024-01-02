95826b71effa8d6b9ddde7e4858979fbcf66c0a6
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-02 16:10:28 +1030

    Time: 0 ms (100.00%) | Memory: 7.7 MB (57.28%) - LeetSync

---
+++ leetcode/362-design-hit-counter/design-hit-counter.cpp
@@ -0,0 +1,27 @@
+class HitCounter {
+public:
+    map<int, int> hit_map{};
+
+    HitCounter() {}
+    
+    void hit(int timestamp) {
+        hit_map[timestamp]++;
+    }
+    
+    int getHits(int timestamp) {
+        int hit_tacker = 0;
+
+        for (const auto& [x, y] : hit_map)
+            if (x > timestamp - 300 && x <= timestamp) 
+                hit_tacker += y;
+
+        return hit_tacker;
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
