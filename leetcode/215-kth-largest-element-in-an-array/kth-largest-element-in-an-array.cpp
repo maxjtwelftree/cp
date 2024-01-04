7501fc7e593dc1028aa8e8d59c3e89cee6adbb7f
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-05 08:53:02 +1030

    Time: 67 ms (97.52%) | Memory: 55.7 MB (81.60%) - LeetSync

--- leetcode/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ leetcode/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,6 +1,5 @@
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
+struct Solution {
+    [[nodiscard]] int findKthLargest(vector<int>& nums, int k) {
         nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
         return nums[k-1];
     }
