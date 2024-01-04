c75b1395fc92c000ab76b735be985eee3cc35e28
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-04 11:41:34 +1030

    Time: 120 ms (61.88%) | Memory: 85 MB (90.06%) - LeetSync

--- leetcode/3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ leetcode/3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -3,7 +3,9 @@ public:
     int minOperations(vector<int>& nums) {
         int counter = 0;
         unordered_map<int, int>mapping{};
-        for (const auto& x : nums) mapping[x]++;
+        for_each(nums.begin(), nums.end(), [&mapping](const int& x) {
+            mapping[x]++;
+        });
         
         for (const auto& [x, y] : mapping) {
             if (y == 1) return -1;
