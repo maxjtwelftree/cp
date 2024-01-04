7a35e6e731cec89d82b297d4a5a92ab1a3661121
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-05 08:50:31 +1030

    Time: 218 ms (6.85%) | Memory: 94.2 MB (5.66%) - LeetSync

--- leetcode/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ leetcode/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        return quickSelect(nums, k);
-    }
-
-    int quickSelect(vector<int>& nums, int k) {
-        auto pivot = nums[rand() % nums.size()];
-        
-        vector<int> less_than_pivot, middle, greater_than_pivot;
-        
-        for (auto check: nums) {
-            if (check > pivot) {
-                less_than_pivot.push_back(check);
-            } else if (check < pivot) {
-                greater_than_pivot.push_back(check);
-            } else {
-                middle.push_back(check);
+        multiset<int> setting{};
+        for (const auto& x : nums)  {
+            setting.insert(x);
+            if (setting.size() > k) {
+                setting.erase(setting.begin());
             }
         }
-        
-        if (k <= less_than_pivot.size()) return quickSelect(less_than_pivot, k);
-        
-        if (less_than_pivot.size() + middle.size() < k) return quickSelect(greater_than_pivot, k - less_than_pivot.size() - middle.size());
-        
-        return pivot;
-        
+        return *setting.begin();
     }
 };
