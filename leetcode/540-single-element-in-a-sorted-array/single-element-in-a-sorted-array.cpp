174f1c615fe0e4bbc18ddd5dee9096a5f0e09452
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-06 10:29:43 +1030

    Time: 38 ms (10.16%) | Memory: 28.4 MB (11.68%) - LeetSync

--- leetcode/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ leetcode/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,12 +1,13 @@
-class Solution {
-public:
+struct Solution {
     int singleNonDuplicate(vector<int>& nums) {
         unordered_map<int, int> mapping{};
-        for (const auto& x : nums) mapping[x]++;
+
+        for (const auto& x : nums) 
+            mapping[x]++;
 
         vector<pair<int, int>> pairings(mapping.begin(), mapping.end());
-        sort(pairings.begin(), pairings.end(), [] (const auto& x, const auto& y) {
-            return x.second > y.second;
+        sort(pairings.begin(), pairings.end(), [] (const auto& a, const auto& b) {
+            return a.second > b.second;
         });
 
         return pairings.back().first;
