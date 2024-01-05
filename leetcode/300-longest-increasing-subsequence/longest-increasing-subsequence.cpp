f17d553ccd58eb8846a954785027f953b77175c5
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-05 17:20:13 +1030

    Time: 251 ms (44.47%) | Memory: 10.8 MB (62.11%) - LeetSync

--- leetcode/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ leetcode/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,12 @@
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        int ans = INT_MIN;
-        vector<int> dp(n, 1);
-        dp[0] = 1;
+struct Solution {
+    [[nodiscard]] int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-            }
-            ans = max(ans, dp[i]);
-        }
-        return ans;
+        for (size_t i = 0; i < nums.size(); ++i)  
+            for (size_t j = 0; j < i; ++j)
+                if (nums[i] > nums[j])
+                    dp[i] = max(dp[i], static_cast<int>(dp[j] + 1));
+
+        return *max_element(dp.begin(), dp.end());
     }
 };
