b11d09f946df64c658ab7e01666440d72003534e
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-06 22:58:54 +1030

    Time: 99 ms (40.15%) | Memory: 38.2 MB (77.66%) - LeetSync

--- leetcode/729-my-calendar-i/my-calendar-i.cpp
+++ leetcode/729-my-calendar-i/my-calendar-i.cpp
@@ -1,11 +1,10 @@
 struct MyCalendar {    
     vector<pair<int, int>> pairings{};
     bool book(int start, int end) {
-        for (const auto& i : pairings) {
-            if (max(i.first, start) < min(i.second, end)) {
+        for (const auto& i : pairings) 
+            if (max(i.first, start) < min(i.second, end)) 
                 return false;
-            }
-        }
+                
         pairings.emplace_back(start, end);
         return true;
     }
