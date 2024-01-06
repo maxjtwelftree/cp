daf97c88e47c160a7ff4e5cd1afcd65f2fbe58f6
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-06 22:04:07 +1030

    Time: 6 ms (32.98%) | Memory: 8.9 MB (97.87%) - LeetSync

---
+++ leetcode/1287-distance-between-bus-stops/distance-between-bus-stops.cpp
@@ -0,0 +1,8 @@
+struct Solution {
+    int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
+        if (start > destination) {swap(start, destination);}
+        int_least64_t total = accumulate(distance.begin(), distance.end(), 0);
+        int_least64_t final_dest = accumulate(distance.begin() + start, distance.begin() + destination, 0);
+        return min(final_dest, total - final_dest);
+    }
+};
