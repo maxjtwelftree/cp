87a89c69d065b574a3aa0dd1dbabd581151b8b5c
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-08 12:31:57 +1030

    Time: 104 ms (36.28%) | Memory: 64.9 MB (88.84%) - LeetSync

--- leetcode/975-range-sum-of-bst/range-sum-of-bst.cpp
+++ leetcode/975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -1,12 +1,12 @@
 struct Solution {
-    void dfs(TreeNode* root, int low, int high, auto& sum) {
+    void dfs(TreeNode* root, int low, int high, auto& sum) const {
         if (root == nullptr) return;
         if (root->val >= low && root->val <= high) sum += root->val;
         if (root->left != nullptr && root->val > low) dfs(root->left, low, high, sum);
         if (root->right != nullptr && root->val < high) dfs(root->right, low, high, sum);
     }
-    
-    [[nodiscard]] int rangeSumBST(TreeNode* root, int low, int high) {
+
+    [[nodiscard]] int rangeSumBST(TreeNode* root, int low, int high) const {
         auto sum = 0;
         dfs(root, low, high, sum);
         return sum;
