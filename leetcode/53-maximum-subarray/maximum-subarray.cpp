00988ec9887aa56a8f989d040664fdffb0571332
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-09 11:57:29 +1030

    Time: 88 ms (46.95%) | Memory: 70.7 MB (6.69%) - LeetSync

--- leetcode/53-maximum-subarray/maximum-subarray.cpp
+++ leetcode/53-maximum-subarray/maximum-subarray.cpp
@@ -1,11 +1,10 @@
-// dp tabulation used
-// time = O(n), space = O(n) 
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
+struct Solution {
+    [[nodiscard]] int maxSubArray(vector<int>& nums) {
         vector<int> dp(nums);
-        for (int i = 1; i < size(nums); i++) 
+
+        for (size_t i = 1; i < nums.size(); ++i) 
             dp[i] = max(nums[i], nums[i] + dp[i-1]);
-        return * max_element(begin(dp), end(dp));
+
+        return *max_element(dp.begin(), dp.end());
     }
 };
