604b2388c181c8bb1e5f58e6934760fd6702ddca
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-10 09:44:43 +1030

    Time: 0 ms (100.00%) | Memory: 6.6 MB (92.44%) - LeetSync

--- leetcode/119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ leetcode/119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        int n = rowIndex;
-        vector<int> dp (n + 1);
+        vector<int> dp(rowIndex + 1);
         dp[0] = 1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = n-1; j >= 0; j--) {
+        for (int i = 0; i < rowIndex; ++i) {
+            for (int j = rowIndex - 1; j >= 0; --j) {
                 dp[j+1] += dp[j];
             }
-        }
-        
+        }        
         return dp;
     }
 };
