41a4e81430214e51f361d132068948c26248a6e3
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-10 10:26:20 +1030

    Time: 2 ms (56.65%) | Memory: 6.5 MB (14.69%) - LeetSync

---
+++ leetcode/96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+struct Solution {
+    [[nodiscard]] int numTrees(int n) {
+        vector<int> catalan(n+1);
+        catalan[0] = 1, catalan[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            catalan[i] = 0;
+            for (int j = 0; j < i; j++) {
+                catalan[i] += catalan[j] * catalan[i-j-1];
+            }
+        }
+        return catalan[n];
+    }
+};
