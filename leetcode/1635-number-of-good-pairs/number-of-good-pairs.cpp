be7429f377e7c0f3e2006c04b6743ccb3d71c2bf
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-11 21:03:56 +1030

    Time: 0 ms (100.00%) | Memory: 7.5 MB (95.44%) - LeetSync

--- leetcode/1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ leetcode/1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int n = nums.size(), cnt = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (nums[i] == nums[j]) cnt++;
-            }
+        int_least32_t cunt = 0; 
+        for (size_t i = 0; i < nums.size(); ++i) {
+            for (size_t j = i+1; j < nums.size(); ++j) {
+                if (nums[i] == nums[j]) cunt = cunt + 1;
+            } 
         }
+
         
-        return cnt;
+        
+        return cunt;
     }
 };
