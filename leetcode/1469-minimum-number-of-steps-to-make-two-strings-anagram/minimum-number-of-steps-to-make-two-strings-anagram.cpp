168b72dbe0e5054a87117f4ba5f5e741bd7001ea
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-13 23:29:37 +1030

    Time: 44 ms (89.88%) | Memory: 14.4 MB (99.49%) - LeetSync

--- leetcode/1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ leetcode/1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,13 +1,13 @@
 struct Solution {
     int minSteps(const std::string& s, const std::string& t) {
-        std::array<int_least32_t, 26> freqs{}; 
+        std::array<int32_t, 26> freqs{}; 
 
         for (const auto& ch : s) freqs[ch - 'a']++;
         for (const auto& ch : t) freqs[ch - 'a']--;
 
-        int_least32_t tracking{0};
-        for (int_least32_t difference : freqs) 
-            if (difference > 0) tracking += difference;
+        int32_t tracking = std::accumulate(freqs.begin(), freqs.end(), 0, [] (int32_t acc, int32_t freq) {
+            return acc + (freq > 0 ? freq : 0);
+        });
         
         return tracking;
     }
