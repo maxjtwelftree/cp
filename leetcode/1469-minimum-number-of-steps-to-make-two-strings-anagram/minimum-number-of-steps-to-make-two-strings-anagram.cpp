09e27fdb3fc9655256cf9e8ec3eb3ac731c1cc3c
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-13 14:46:11 +1030

    Time: 70 ms (49.19%) | Memory: 17.2 MB (10.22%) - LeetSync

--- leetcode/1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ leetcode/1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -7,12 +7,9 @@ struct Solution {
         for (const auto& x : s) map1[x]++;
         for (const auto& x : t) map2[x]++;
 
-        for (auto [first, second] : map1) {
+        for (auto [first, second] : map1) 
             if (map2[first] < second) tracking += second - map2[first];
-        }
 
-        // map1 = [a = 1, b = 2]
-        // map2 = [a = 2, b = 1] 
         return tracking;
     }
 };
