d02448a2f30b2e3fed629c0d885181e308225edb
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-14 10:56:28 +1030

    Time: 72 ms (82.37%) | Memory: 20.8 MB (96.46%) - LeetSync

--- leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -2,23 +2,22 @@ struct Solution {
     bool closeStrings(string word1, string word2) {
         if (word1.size() != word2.size()) return false;
 
-        array<int, 26> freq1{}, freq2{}; 
-        array<bool, 26> exists1{}, exists2{};
+        array<int, 26> freq_1{}, freq_2{}; 
+        array<bool, 26> exists_1{}, exists_2{};
 
         for (auto c : word1) {
-            freq1[c - 'a']++;
-            exists1[c - 'a'] = true;
+            freq_1[c - 'a']++;
+            exists_1[c - 'a'] = true;
         }
         
         for (auto c : word2) {
-            freq2[c - 'a']++;
-            exists2[c - 'a'] = true;
+            freq_2[c - 'a']++;
+            exists_2[c - 'a'] = true;
         }
         
-        if (exists1 != exists2) return false;
+        if (exists_1 != exists_2) return false;
+        ranges::sort(freq_1), ranges::sort(freq_2);
 
-        ranges::sort(freq1), ranges::sort(freq2);
-
-        return freq1 == freq2;
+        return freq_1 == freq_2;
     }
 };
