1db01df6bc3b9c6e55fb4f234e26cc279be432e5
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-14 10:55:40 +1030

    Time: 69 ms (86.05%) | Memory: 20.7 MB (96.46%) - LeetSync

--- leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -2,26 +2,22 @@ struct Solution {
     bool closeStrings(string word1, string word2) {
         if (word1.size() != word2.size()) return false;
 
-        array<int, 26> freq1{}; // Frequency array for word1
-        array<int, 26> freq2{}; // Frequency array for word2
-        array<bool, 26> exists1{}; // Existence array for word1
-        array<bool, 26> exists2{}; // Existence array for word2
+        array<int, 26> freq1{}, freq2{}; 
+        array<bool, 26> exists1{}, exists2{};
 
-        for (char c : word1) {
+        for (auto c : word1) {
             freq1[c - 'a']++;
             exists1[c - 'a'] = true;
         }
-        for (char c : word2) {
+        
+        for (auto c : word2) {
             freq2[c - 'a']++;
             exists2[c - 'a'] = true;
         }
-
-        // Check if both strings contain the same unique characters
+        
         if (exists1 != exists2) return false;
 
-        // Sort and compare frequencies
-        sort(freq1.begin(), freq1.end());
-        sort(freq2.begin(), freq2.end());
+        ranges::sort(freq1), ranges::sort(freq2);
 
         return freq1 == freq2;
     }
