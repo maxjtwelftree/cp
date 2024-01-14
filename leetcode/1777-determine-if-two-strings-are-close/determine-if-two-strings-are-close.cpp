e8cb6a00d777244b5c3d0c834002a4ac34ffb5e8
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-14 10:53:28 +1030

    Time: 102 ms (63.65%) | Memory: 21 MB (60.02%) - LeetSync

--- leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,17 +1,13 @@
-class Solution {
-public:
+struct Solution {
     bool closeStrings(string word1, string word2) {
         if (word1.size() != word2.size()) return false;
 
-        unordered_map<char, int> mapping_1{};
-        unordered_map<char, int> mapping_2{};
+        unordered_map<char, int> mapping_1{}, mapping_2{};;
+        for (auto c : word1) mapping_1[c]++;
+        for (auto c : word2) mapping_2[c]++;
 
-        for (const auto c : word1) mapping_1[c]++;
-        for (const auto c : word2) mapping_2[c]++;
-
-        for (const auto& i : mapping_1) {
+        for (const auto& i : mapping_1) 
             if (mapping_2.find(i.first) == mapping_2.end()) return false;
-        }
 
         vector<pair<int, int>> out1 (mapping_1.begin(), mapping_1.end());
         sort(out1.begin(), out1.end(), [] (const auto a, const auto b) {
