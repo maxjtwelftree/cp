f954b527b8d070f044b91fb8fc0701a9a4ea9051
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-16 11:23:04 +1030

    Time: 206 ms (25.62%) | Memory: 97.3 MB (85.99%) - LeetSync

--- leetcode/380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ leetcode/380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,6 +1,5 @@
 struct RandomizedSet {
     unordered_set<int> random_set;
-
     mutable default_random_engine engine;
     mutable uniform_int_distribution<size_t> dist;
 
@@ -11,8 +10,6 @@ struct RandomizedSet {
     bool remove(int val) {return random_set.erase(val) > 0;}
     
     int getRandom() const {
-        if (random_set.empty()) throw runtime_error("Set is empty");
-
         dist.param(uniform_int_distribution<size_t>::param_type(0, random_set.size() - 1));
         auto it = next(random_set.cbegin(), dist(engine));
         return *it;
