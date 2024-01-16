c2bf71cae0a87413702dc16ec12fb05e98471846
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-16 13:55:59 +1030

    Time: 171 ms (67.42%) | Memory: 97.5 MB (30.22%) - LeetSync

--- leetcode/380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ leetcode/380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,17 +1,38 @@
-struct RandomizedSet {
-    unordered_set<int> random_set;
-    mutable default_random_engine engine;
-    mutable uniform_int_distribution<size_t> dist;
+#include <vector>
+#include <unordered_map>
+#include <random>
 
-    RandomizedSet() : engine(random_device{}()) {}
+class RandomizedSet {
+    std::vector<int> vec;
+    std::unordered_map<int, int> map;
+    std::default_random_engine engine;
 
-    bool insert(int val) {return random_set.insert(val).second;}
-    
-    bool remove(int val) {return random_set.erase(val) > 0;}
-    
-    int getRandom() const {
-        dist.param(uniform_int_distribution<size_t>::param_type(0, random_set.size() - 1));
-        auto it = next(random_set.cbegin(), dist(engine));
-        return *it;
+public:
+    RandomizedSet() : engine(std::random_device{}()) {}
+
+    bool insert(int val) {
+        if (map.find(val) != map.end()) {
+            return false;
+        }
+        vec.push_back(val);
+        map[val] = vec.size() - 1;
+        return true;
+    }
+
+    bool remove(int val) {
+        if (map.find(val) == map.end()) {
+            return false;
+        }
+        int lastElement = vec.back();
+        map[lastElement] = map[val];
+        vec[map[val]] = lastElement;
+        vec.pop_back();
+        map.erase(val);
+        return true;
+    }
+
+    int getRandom() {
+        std::uniform_int_distribution<int> distribution(0, vec.size() - 1);
+        return vec[distribution(engine)];
     }
 };
