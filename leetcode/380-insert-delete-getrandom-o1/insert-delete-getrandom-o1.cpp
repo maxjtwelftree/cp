5ef7106f25358e9e98560ab89f46a76dd7730c6f
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-16 11:17:35 +1030

    Time: 195 ms (31.68%) | Memory: 97.3 MB (73.46%) - LeetSync

--- leetcode/380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ leetcode/380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,29 +1,20 @@
-#include <unordered_set>
-#include <random>
-
 struct RandomizedSet {
-    std::unordered_set<int> random_set;
+    unordered_set<int> random_set;
 
-    mutable std::default_random_engine engine;
-    mutable std::uniform_int_distribution<std::size_t> dist;
+    mutable default_random_engine engine;
+    mutable uniform_int_distribution<size_t> dist;
 
-    RandomizedSet() : engine(std::random_device{}()) {}
+    RandomizedSet() : engine(random_device{}()) {}
 
-    bool insert(int val) {
-        return random_set.insert(val).second;
-    }
+    bool insert(int val) {return random_set.insert(val).second;}
     
-    bool remove(int val) {
-        return random_set.erase(val) > 0;
-    }
+    bool remove(int val) {return random_set.erase(val) > 0;}
     
     int getRandom() const {
-        if (random_set.empty()) {
-            throw std::runtime_error("Set is empty");
-        }
+        if (random_set.empty()) throw runtime_error("Set is empty");
 
-        dist.param(std::uniform_int_distribution<std::size_t>::param_type(0, random_set.size() - 1));
-        auto it = std::next(random_set.cbegin(), dist(engine));
+        dist.param(uniform_int_distribution<size_t>::param_type(0, random_set.size() - 1));
+        auto it = next(random_set.cbegin(), dist(engine));
         return *it;
     }
 };
