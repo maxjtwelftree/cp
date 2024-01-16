5e9d53787654ae23fc088bda6033ac14ddc05c42
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-16 18:59:39 +1030

    Time: 0 ms (100.00%) | Memory: 8.3 MB (19.46%) - LeetSync

--- leetcode/198-house-robber/house-robber.cpp
+++ leetcode/198-house-robber/house-robber.cpp
@@ -1,20 +1,14 @@
-class Solution {
-public:
-    int rob(vector<int>& nums) {
+struct Solution {
+    [[nodiscard]] int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n);
-    
-        if (n == 1) {
-            return nums[0];
-        }
+        vector<int>dp(n+1);
+        dp[0] = 0;
+        dp[1] = nums[0];
         
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1]);
         }
 
-        return dp.back();      
+        return dp[n];
     }
 };
