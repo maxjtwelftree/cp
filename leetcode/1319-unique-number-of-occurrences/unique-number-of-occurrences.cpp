d17fd5f692f0ecd61728c39c3d5cb0c9b6cee295
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-01-17 10:56:26 +1030

    Time: 0 ms (100.00%) | Memory: 8.8 MB (9.43%) - LeetSync

--- leetcode/1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ leetcode/1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> map;
-        for (auto num : arr) {
-            map[num]++;
-        }
+        unordered_map<int, int> mapping;
 
-        unordered_set<int> freq;
-        for(auto it: map) {
-            freq.insert(it.second);
-        } 
-        if (map.size()==freq.size()) {
-            return true;
+        for (auto x : arr) mapping[x]++;
+
+
+        vector<pair<int, int>> out(mapping.begin(), mapping.end());
+        sort(out.begin(), out.end(), [] (const auto x, const auto y) {
+            return x.second > y.second;
+        });
+
+        for (int i = 1; i < out.size(); ++i) {
+            if (out[i].second == out[i-1].second) return false;
         }
-        return false;
+        return true;
     }
 };
