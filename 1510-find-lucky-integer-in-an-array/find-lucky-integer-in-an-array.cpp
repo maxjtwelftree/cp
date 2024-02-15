530b83bf9eaf5246f165bd3a102ba8b016c0753b
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-02-15 17:06:07 +1030

    Time: 4 ms (74.85%) | Memory: 14 MB (7.09%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -1,13 +1,13 @@
 struct Solution {
-    int findLucky(vector<int>& arr) {
-        vector<int> lucky_nums{};
-        unordered_map<int, int> mapping{};
+    [[nodiscard]] int_least32_t findLucky(vector<int>& arr) {
+        vector<int_least32_t> lucky_nums{};
+        unordered_map<int_least32_t, int_least32_t> mapping{};
 
-        for (const auto& x : arr) 
-            mapping[x]++;
+        for (const auto& element : arr) 
+            mapping[element]++;
         
-        for (auto [x, y] : mapping) 
-            if (x == y) lucky_nums.push_back(x);
+        for (auto [key, value] : mapping) 
+            if (key == value) lucky_nums.push_back(key);
         
         return !lucky_nums.empty() ? *max_element(lucky_nums.begin(), lucky_nums.end()) : -1;   
     }
