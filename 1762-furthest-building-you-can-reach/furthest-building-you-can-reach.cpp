73c1801c1c9c763de650980accbfaba5ce0c6c5d
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-02-17 12:05:50 +1030

    Time: 88 ms (28.03%) | Memory: 57.1 MB (53.87%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int32_t, vector<int32_t>, greater<int32_t>> pq;
+        
+        for (size_t i = 1; i < heights.size(); ++i) {
+            if (heights[i] - heights[i-1] > 0) {pq.push(heights[i] - heights[i-1]);
+                if (pq.size() > ladders) {bricks -= pq.top(); pq.pop();}}
+            if (bricks < 0) {return i - 1;}
+        }  
+
+        return heights.size() - 1;
+    }
+};
