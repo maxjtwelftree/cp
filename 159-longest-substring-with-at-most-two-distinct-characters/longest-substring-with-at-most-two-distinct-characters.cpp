ff31b9933eb8a87c62d40289a5c2f6f61a657d2e
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-02-19 16:21:11 +1030

    Time: 145 ms (58.69%) | Memory: 37 MB (24.03%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.cpp
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.cpp
@@ -1,15 +1,16 @@
 struct Solution {
-    [[nodiscard]] int lengthOfLongestSubstringTwoDistinct(string s) const {
-        unordered_map<char, int> mapping{};
-        int32_t final_ans = 0, left = 0;
+    [[nodiscard]] int_least32_t lengthOfLongestSubstringTwoDistinct(string s) const {
+        unordered_map<char, int_least32_t> mapping{};
+        int_least32_t final_ans = 0;
+        int_least32_t left = 0;
 
-        for (int right = 0; right < s.length(); ++right) {
+        for (size_t right = 0; right < s.length(); ++right) {
             mapping[s[right]] = right;
             while (mapping.size() > 2) {
                 if (mapping[s[left]] == left) mapping.erase(s[left]);
                 left++;
             }
-            final_ans = max(final_ans, right - left + 1);
+            final_ans = max(final_ans, static_cast<int_least32_t>(right) - left + 1);
         }
         
         return final_ans;
