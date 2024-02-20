0f4d2415b74ec76524f8ff964b7bb8853b7670d9
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-02-20 21:55:14 +1030

    Time: 19 ms (30.92%) | Memory: 20.4 MB (32.85%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,11 +1,5 @@
 struct Solution {
     [[nodiscard]] const int missingNumber(const vector<int>& nums) const {
-        int_least32_t answer = nums.size();
-
-        for (size_t i = 0; i < nums.size(); ++i) {
-            answer ^= i ^ nums[i];
-        }
-        
-        return answer;
+        int_least32_t answer = nums.size(); for (size_t i = 0; i < nums.size(); ++i) answer ^= i ^ nums[i]; return answer;
     }
 };
