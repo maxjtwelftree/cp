a09d503e8486b28384a5bae2ed8d65e7bdb21635
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-02-22 12:44:14 +1030

    Time: 122 ms (83.80%) | Memory: 64.2 MB (73.94%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,19 +1,11 @@
-class Solution {
-public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        if (n == 1) return 1;
-        unordered_map<int, int> mapping{};
-
-        for (const auto& x : trust) {
-            mapping[x[0]]--, mapping[x[1]]++;
+struct Solution {
+        int findJudge(int N, vector<vector<int>>& trust) {
+        vector<int> count(N + 1, 0);
+        for (auto& t : trust)
+            count[t[0]]--, count[t[1]]++;
+        for (int i = 1; i <= N; ++i) {
+            if (count[i] == N - 1) return i;
         }
-
-        for (int i = 0; i <= n; ++i) {
-            if (mapping[i] == n - 1) {
-                return i;
-            }
-        }
-
-        return n == 1 ? 1 : -1;
+        return -1;
     }
 };
