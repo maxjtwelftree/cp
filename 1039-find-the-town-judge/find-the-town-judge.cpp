2fe11e123c201fbaab8d8785b9b8b0e2a8601772
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-02-22 22:49:34 +1030

    Time: 105 ms (97.70%) | Memory: 64.3 MB (69.38%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,11 +1,12 @@
 struct Solution {
-    int findJudge(int_least32_t n, vector<vector<int_least32_t>>& trust) const {
+    int_least32_t findJudge(const int_least32_t n, const vector<vector<int_least32_t>>& trust) const {
         vector<int_least32_t> mapping(n + 1, 0);
+        // maybe reserve called here instead of above?
 
         for (const auto& x : trust) 
             mapping[x[0]]--, mapping[x[1]]++;
         
-        for (int i = 1; i <= n; ++i) 
+        for (int_least32_t i = 1; i <= n; ++i) 
             if (mapping[i] == n - 1) return i;
         
         return -1;
