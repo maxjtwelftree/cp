72800fe524da76d70bacd41a7b6e58e53b5cadab
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-03-09 12:24:55 +1030

    Time: 106 ms (24.23%) | Memory: 69 MB (18.41%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set(nums2.begin(), nums2.end());
-        
-        for (auto a : nums1) {
-            if (set.contains(a)) return a;
-        }
-        
+        unordered_set<int> set(nums2.cbegin(), nums2.cend());
+        for (auto a : nums1) if (set.contains(a)) return a;
         return -1;
     }
 };
