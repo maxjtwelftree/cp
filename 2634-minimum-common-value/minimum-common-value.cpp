ff23e46b6eb6e56b83d57e5abb94d08d1f03eb43
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-03-09 12:25:45 +1030

    Time: 112 ms (21.89%) | Memory: 69.1 MB (18.41%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,7 +1,7 @@
 struct Solution {
-    [[nodiscard]] int getCommon(vector<int>& nums1, vector<int>& nums2) {
+    [[nodiscard]] int getCommon(vector<int>& nums1, vector<int>& nums2) const {
         auto set = unordered_set<int>(nums2.cbegin(), nums2.cend());
-        for (auto a : nums1) if (set.contains(a)) return a;
+        for (const auto& a : nums1) if (set.contains(a)) return a;
         return -1;
     }
 };
