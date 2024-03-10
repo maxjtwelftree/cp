21e947b2286f227f770c7cff948f2a9c1e2a74a8
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-03-10 11:32:52 +1030

    Time: 4 ms (68.61%) | Memory: 13.1 MB (32.08%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,11 +1,11 @@
 struct Solution {
     vector<int> intersection(const vector<int>& nums1, const vector<int>& nums2) {
-        unordered_set<int> set(nums1.begin(), nums1.end()); // Create a set from nums1
+        unordered_set<int> set(nums1.begin(), nums1.end()); 
         vector<int> result;
 
-        // Filter elements from nums2, adding unique ones to the result
-        std::copy_if(nums2.begin(), nums2.end(), std::back_inserter(result),
-                    [&](int num) { return set.erase(num); }); 
+        copy_if(nums2.begin(), nums2.end(), std::back_inserter(result), [&](auto num) { 
+            return set.erase(num); 
+        }); 
 
         return result;
     }
