91a01522e3277d33772f2b51ae44b561e0a27389
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-03-11 10:55:38 +1030

    Time: 5 ms (8.56%) | Memory: 8.7 MB (17.36%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,6 +1,5 @@
-class Solution {
-public:
-    string customSortString(string order, string s) {
+struct Solution {
+    string customSortString(string order, string s) const {
         sort(s.begin(), s.end(), [order] (auto char_1, auto char_2) {
             return order.find(char_1) < order.find(char_2);
         });
