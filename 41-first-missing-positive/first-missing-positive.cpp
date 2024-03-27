3f5788ccd46239f3dd1ee548cfeeade2d311ec9d
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-03-27 11:35:25 +1030

    Time: 27 ms (98.08%) | Memory: 43.5 MB (81.31%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) if (nums[i] <= 0) nums[i] = nums.size() + 1;
+        for (int i = 0; i < nums.size(); ++i) if (abs(nums[i]) <= nums.size() && nums[abs(nums[i]) - 1] > 0) nums[abs(nums[i]) - 1] *= -1;
+        for (int i = 0; i < nums.size(); ++i) if (nums[i] > 0) return i + 1;
+        return nums.size() + 1;
+    }
+};
