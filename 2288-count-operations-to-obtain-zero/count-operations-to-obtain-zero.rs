6ffa0b4d6f200de71544847e09bd8fb96d54f74c
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-04-03 11:07:42 +1030

    Time: 1 ms (76.19%) | Memory: 2.2 MB (14.29%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn count_operations(mut num1: i32, mut num2: i32) -> i32 {
+        let (mut x, mut y) = (0, 0);
+        while num1 > 0 && num2 > 0 {
+            match num1 < num2 {
+                true => num2 -= num1,
+                false => num1 -= num2,
+            }
+            y += 1;
+        }
+        y
+    }
+}
