91271e4f5c25d14c27f72ffb5bc718902d87a18c
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2024-04-04 11:12:12 +1030

    Time: 1 ms (86.36%) | Memory: 2 MB (81.82%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.rs
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.rs
@@ -1,12 +1,12 @@
 impl Solution {
     pub fn max_depth(s: String) -> i32 {
-        s.chars().fold((0, 0), |(depth_cnt, final_cnt), c| {
-            let depth_cnt = match c {
-                '(' => depth_cnt + 1,
-                ')' => depth_cnt - 1,
-                _ => depth_cnt,
-            };
-            (depth_cnt, final_cnt.max(depth_cnt))
-        }).1
+        s.chars().map(|c| if c == '(' { 1 } else if c == ')' { -1 } else { 0 })
+            .scan((0, 0), |(depth, max_depth), change| {
+                *depth += change;
+                *max_depth = (*max_depth).max(*depth);
+                Some(*max_depth)
+            })
+            .last()
+            .unwrap_or(0)
     }
 }
